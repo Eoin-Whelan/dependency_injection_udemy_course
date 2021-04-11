0ca7a6bafffdc8d0dcfdb42e200f98d774c83de2
EoinFar <eoinfar75@gmail.com>
2021-04-11 10:19:49 +0100

    First commit of dependency injection. - Created examples of high/low coupling class design. - Created interfaces to allow for reduced coupling of concrete classes.
    Upcoming:
    - Implementation of IServiceCollection/IServiceProvider to further understanding
    of dependency injection via those methods.

---
+++ Coupling/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DependencyInjectionCourse;
+
+namespace Coupling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataAccess dal = new DataAccess();
+            IBusiness bidness = new Business(dal);
+            var userInterface = new UserInterface(bidness);
+        }
+    }
+}
---
+++ DependencyInjectionCourse/ProgramWithHighCoupling.cs
@@ -0,0 +1,49 @@
+﻿///*
+// *  Point of this project is to explain class coupling. 
+// */
+
+//using System;
+
+//namespace DependencyInjectionCourse
+//{
+//    class ProgramWithHighCoupling
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+
+//    public class UserInterface
+//    {
+//        public void GetData()
+//        {
+//            Console.WriteLine("Please enter your username");
+//            var userName = Console.ReadLine();
+
+//            Console.WriteLine("Please enter your password");
+//            var password = Console.ReadLine();
+
+//            var business = new Business();
+//            business.signup(userName, password);
+//        }
+//    }
+
+//    public class Business
+//    {
+//        public void signup(string username, string password)
+//        {
+//            //  Validation
+//            var dal = new DataAccess();
+//            dal.store(username, password);
+//        }
+//    }
+
+//    public class DataAccess
+//    {
+//        public void store(string username, string password)
+//        {
+//            //  Writes to DB
+//        }
+//    }
+//}
